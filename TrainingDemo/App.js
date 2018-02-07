9fd51bd66b4b2efa9d195c382bd04c70541ca9bb
DeqingQu <qu.deqing@gmail.com>
2018-02-06 19:23:46 -0800

    HelloWorld components

--- TrainingDemo/App.js
+++ TrainingDemo/App.js
@@ -12,6 +12,8 @@ import {
   View
 } from 'react-native';
 
+import HelloWorldComponent from "./src/HelloWorld"
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
@@ -24,19 +26,13 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <HelloWorldComponent />
       </View>
     );
   }
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
