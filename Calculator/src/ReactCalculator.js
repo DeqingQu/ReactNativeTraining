6a9cf4870720d8516fbfd7ff0883fdf095eeabef
DeqingQu <qu.deqing@gmail.com>
2018-02-07 14:11:50 -0800

    calculator component

---
+++ Calculator/src/ReactCalculator.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {
+    Text,
+    AppRegistry
+} from 'react-native';
+
+export default class ReactCalculator extends Component {
+
+    render() {
+        return (
+            <Text>Hello, React!</Text>
+        )
+    }
+
+}
