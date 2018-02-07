e7b9f05000692c0293d2d4af7c816bcdbb2009c5
DeqingQu <qu.deqing@gmail.com>
2018-02-06 22:12:46 -0800

    three methods in flex layout

--- TrainingDemo/src/FlexDirectionBasics.js
+++ TrainingDemo/src/FlexDirectionBasics.js
@@ -5,7 +5,21 @@ export default class FlexDirectionBasics extends Component {
   render() {
     return (
       // Try setting `flexDirection` to `column`.
-      <View style={{flex: 1, flexDirection: 'column'}}>
+
+      // <View style={{flex: 1, flexDirection: 'column'}}>
+
+      // <View style={{
+      //   flex: 1,
+      //   flexDirection: 'column',
+      //   justifyContent: 'space-between',
+      // }}>
+
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
         <View style={{width: 150, height: 150, backgroundColor: 'powderblue'}} />
         <View style={{width: 150, height: 150, backgroundColor: 'skyblue'}} />
         <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
