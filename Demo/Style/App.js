f051c1bc51033c48cc0ec62596c3c47daa392bfe
DeqingQu <qu.deqing@gmail.com>
2018-02-24 12:30:20 -0800

    demo style

---
+++ Demo/Style/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import StyleComponent from "./src/Style"
+
+export default class App extends React.Component {
+  render() {
+    return (
+          /*  Style   */
+          <StyleComponent />
+    );
+  }
+}
---
+++ Demo/Style/src/Style.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class StyleComponent extends Component {
+  render() {
+    return (
+        <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text style={styles.red}>just red</Text>
+            <Text style={styles.bigblue}>just bigblue</Text>
+            <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
+            <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
+        </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    bigblue: {
+        color: 'blue',
+        fontWeight: 'bold',
+        fontSize: 30,
+    },
+    red: {
+        color: 'red',
+    },
+});
