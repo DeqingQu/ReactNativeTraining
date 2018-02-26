59a7fa2ef308796fe52b4b21e1462e6d744f1154
DeqingQu <qu.deqing@gmail.com>
2018-02-26 12:36:23 -0800

    add ex2-solution

--- Excercise/ex2-solution/App.js
+++ Excercise/ex2-solution/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Counter from "./src/Counter"
-// import EvenCounter from "./src/EvenCounter"
+import EvenCounter from "./src/EvenCounter"
 
 export default class App extends React.Component {
     render() {
       return (
             <View style={styles.container}>
                 <Counter text="I love counter: " />
+                <EvenCounter text="I love even counter: " />
             </View>
       );
     }
